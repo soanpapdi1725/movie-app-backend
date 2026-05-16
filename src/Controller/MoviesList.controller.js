f87732c8b40f537229d60c988ac26e99f4302b33
Sonu Yadav <sy239370@gmail.com>
2026-05-16 20:45:29 +0530

    query from params

--- src/Controller/MoviesList.controller.js
+++ src/Controller/MoviesList.controller.js
@@ -28,7 +28,7 @@ export const getMovies = async (req, res) => {
 };
 export const searchMovie = async (req, res) => {
   try {
-    const { query } = req.body;
+    const { query } = req.params;
     const endpoint = `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`;
 
     const response = await apiConnector(
