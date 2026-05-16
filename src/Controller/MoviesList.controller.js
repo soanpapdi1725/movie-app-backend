6f272a1b73dc4a6c49b6d145ee8a75d8a4c84ef1
Sonu Yadav <sy239370@gmail.com>
2026-05-16 20:00:40 +0530

    done

--- src/Controller/MoviesList.controller.js
+++ src/Controller/MoviesList.controller.js
@@ -6,7 +6,7 @@ export const getMovies = async (req, res) => {
   try {
     
     const query = req.body?.query;
-
+    console.log(query)
     const endpoint = query
       ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
       : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
